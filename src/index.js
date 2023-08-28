e504e55ceaa375e5814e3e1c335d951b2e715c1d
unknown <grandalf6@gmail.com>
2023-08-28 13:37:45 +0100

    Fix more button

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 // -* Data should comming from https://www.weatherapi.com/docs/
-
+//
 // HOME WORK
 // - Next to units, there should be button 'Show' favs', after pressing it should become 'Show all' and only favs cities should be shown in grid (units, more/less and searching should still work on favs)
 
-const CONTROL_BUTTON_LABEL = {
-  more: 'More',
-  less: 'Less',
-};
-
-const UNITS_BUTTON_LABEL = {
-  metric: 'Metric',
-  imperial: 'Imperial',
-};
-
+// CONFIG
 const DATA = [
   {
     cityName: 'Southend-on-Sea',
@@ -124,243 +115,279 @@ const DATA = [
     windInKph: 80,
     conditionText: 'cloudy',
   },
-];
+]
 
-const ALFA = 'Show favourites';
-const BETA = 'Show all';
+const BUTTON_LABELS = {
+  controls: {
+    less: 'Less',
+    more: 'More',
+  },
+  unitButtons: {
+    metric: 'Metric',
+    imperial: 'Imperial',
+  },
+  showAll: 'Show all',
+  showFavs: 'Show favourites',
+}
 
-(() => {
-  const unitSwitchButtonElement = document.getElementById('units-button');
-  const moreButtonElement = document.getElementById('more-or-less-button');
-  const mainElement = document.querySelector('main');
-  const searchFieldElement = document.getElementById('search-field');
-  const searchButtonElement = document.querySelector('#search-button');
-  const clearButtonElement = document.querySelector('#clear-button');
-  const favouriteButtonElement = document.querySelector('#favourite-button');
+//
 
-  let favouriteCities = [];
+// CONSTANTS
+const unitSwitchButtonElement = document.getElementById('units-button')
+const moreButtonElement = document.getElementById('more-or-less-button')
+const mainElement = document.querySelector('main')
+const searchFieldElement = document.getElementById('search-field')
+const searchButtonElement = document.querySelector('#search-button')
+const clearButtonElement = document.querySelector('#clear-button')
+const favouriteButtonElement = document.querySelector('#favourite-button')
+//
 
-  clearButtonElement.disabled = true;
-
-  const getShouldDisplayImperial = () =>
-    unitSwitchButtonElement.innerText === UNITS_BUTTON_LABEL.metric;
-  const getShouldShowAll = () =>
-    moreButtonElement.innerText === CONTROL_BUTTON_LABEL.less;
-  const getSearchQuery = () => searchFieldElement.value;
-  const getShouldShowFavsOnly = () =>
-    favouriteButtonElement.innerText === ALFABETA;
-
+// HELPERS
+const getShouldDisplayImperial = () =>
+  unitSwitchButtonElement.innerText === BUTTON_LABELS.unitButtons.metric
+const getSearchQuery = () => searchFieldElement.value
+const getShouldShowAll = () =>
+  moreButtonElement.innerText === BUTTON_LABELS.controls.less
+const getShouldShowFavsOnly = () =>
+  favouriteButtonElement.innerText === BUTTON_LABELS.showAll
+//
+;(() => {
+  // HELPERS
   const renderTiles = (
     shouldShowAll,
     shouldDisplayImperial,
     searchQuery,
     shouldShowFavsOnly
   ) => {
-    const oldTiles = document.querySelectorAll('.tile');
-    oldTiles.forEach((oldTile) => oldTile.remove());
+    // Remove existing tiles
+    document.querySelectorAll('.tile').forEach((oldTile) => oldTile.remove())
 
-    const filteredData = DATA.filter((city) => {
-      const isThisCityFavourite = favouriteCities.includes(city.cityName);
+    // Render new tiles
+    DATA.filter((city) => {
+      const isThisCityFavourite = favouriteCities.includes(city.cityName)
+      const favsCondition = shouldShowFavsOnly ? isThisCityFavourite : true
+      const queryCondition = searchQuery
+        ? city.cityName.toLowerCase().includes(searchQuery.toLowerCase())
+        : true
 
-      let favsCondition;
-      if (shouldShowFavsOnly) {
-        favsCondition = isThisCityFavourite;
-      } else {
-        favsCondition = true;
-      }
-
-      let queryCondition;
-      if (searchQuery) {
-        queryCondition = city.cityName
-          .toLowerCase()
-          .includes(searchQuery.toLowerCase());
-      } else {
-        queryCondition = true;
-      }
-
-      return queryCondition && favsCondition;
-    });
-    filteredData.forEach((tile, index) => {
-      const shouldThisTileBeVisible = shouldShowAll ? true : index < 8;
-      const shouldDisplayImperial = getShouldDisplayImperial();
+      return queryCondition && favsCondition
+    }).forEach((tile, index) => {
+      const shouldThisTileBeVisible = shouldShowAll ? true : index < 8
 
       // TITLES WRAPPER
-      const titlesWrapperElement = document.createElement('div');
-      titlesWrapperElement.className = 'titles-wrapper';
+      const titlesWrapperElement = document.createElement('div')
+      titlesWrapperElement.className = 'titles-wrapper'
 
-      const tileCityElement = document.createElement('h2');
-      tileCityElement.innerHTML = tile.cityName;
-      titlesWrapperElement.appendChild(tileCityElement);
+      const tileCityElement = document.createElement('h2')
+      tileCityElement.innerHTML = tile.cityName
+      titlesWrapperElement.appendChild(tileCityElement)
 
-      const tileCountryElement = document.createElement('p');
-      tileCountryElement.innerHTML = tile.countryName;
-      titlesWrapperElement.appendChild(tileCountryElement);
+      const tileCountryElement = document.createElement('p')
+      tileCountryElement.innerHTML = tile.countryName
+      titlesWrapperElement.appendChild(tileCountryElement)
       //
 
       // HEADER
-      const tileHeaderElement = document.createElement('header');
-      const tileIconElement = document.createElement('div');
-      tileIconElement.className = 'weather-icon';
+      const tileHeaderElement = document.createElement('header')
+      const tileIconElement = document.createElement('div')
+      tileIconElement.className = 'weather-icon'
 
-      tileHeaderElement.appendChild(titlesWrapperElement);
-      tileHeaderElement.appendChild(tileIconElement);
+      tileHeaderElement.appendChild(titlesWrapperElement)
+      tileHeaderElement.appendChild(tileIconElement)
       //
 
+      // TEMP
+      const TILE_LABELS = {
+        _separator: ': ',
+        condition: 'Condition',
+        humidity: 'Humidity',
+        precipitation: 'Precipitation',
+        temperature: {
+          inC: 'Temperature in C',
+          inF: 'Temperature in F',
+        },
+        wind: {
+          inKpH: 'Wind in KpH',
+          inMpH: 'Wind in MpH',
+        },
+      }
+
       // DATA WRAPPER
-      const tileDataWrapperElement = document.createElement('main');
+      const tileDataWrapperElement = document.createElement('main')
 
-      const tileTemperatureElement = document.createElement('p');
-      if (shouldDisplayImperial) {
-        tileTemperatureElement.innerHTML =
-          'Temperature in F: ' + tile.temperatureInF;
-      } else {
-        tileTemperatureElement.innerHTML =
-          'Temperature in C: ' + tile.temperatureInC;
-      }
+      const tileTemperatureElement = document.createElement('p')
+      tileTemperatureElement.innerHTML =
+        TILE_LABELS.temperature[shouldDisplayImperial ? 'inF' : 'inC'] +
+        TILE_LABELS._separator +
+        tile[shouldDisplayImperial ? 'temperatureInF' : 'temperatureInC']
 
-      const tilePrecipitationElement = document.createElement('p');
+      const tilePrecipitationElement = document.createElement('p')
       tilePrecipitationElement.innerHTML =
-        'Precipitation: ' + tile.precipitation;
+        TILE_LABELS.precipitation + TILE_LABELS._separator + tile.precipitation
 
-      const tileHumidityElement = document.createElement('p');
-      tileHumidityElement.innerHTML = 'Humidity: ' + tile.humidity;
+      const tileHumidityElement = document.createElement('p')
+      tileHumidityElement.innerHTML =
+        TILE_LABELS.humidity + TILE_LABELS._separator + tile.humidity
 
-      const tileWindInMphElement = document.createElement('p');
-      if (shouldDisplayImperial) {
-        tileWindInMphElement.innerHTML = 'Wind in MpH: ' + tile.windInMph;
-      } else {
-        tileWindInMphElement.innerHTML = 'Wind in KpH: ' + tile.windInKph;
-      }
+      const tileWindInMphElement = document.createElement('p')
+      tileWindInMphElement.innerHTML =
+        TILE_LABELS.wind[shouldDisplayImperial ? 'inMpH' : 'inKpH'] +
+        TILE_LABELS._separator +
+        tile[shouldDisplayImperial ? 'windInMph' : 'windInKph']
 
-      const tileConditionTextElement = document.createElement('p');
-      tileConditionTextElement.innerHTML = 'Condition: ' + tile.conditionText;
+      const tileConditionTextElement = document.createElement('p')
+      tileConditionTextElement.innerHTML =
+        TILE_LABELS.condition + TILE_LABELS._separator + tile.conditionText
 
-      tileDataWrapperElement.appendChild(tileTemperatureElement);
-      tileDataWrapperElement.appendChild(tilePrecipitationElement);
-      tileDataWrapperElement.appendChild(tileHumidityElement);
-      tileDataWrapperElement.appendChild(tileWindInMphElement);
-      tileDataWrapperElement.appendChild(tileConditionTextElement);
+      tileDataWrapperElement.appendChild(tileTemperatureElement)
+      tileDataWrapperElement.appendChild(tilePrecipitationElement)
+      tileDataWrapperElement.appendChild(tileHumidityElement)
+      tileDataWrapperElement.appendChild(tileWindInMphElement)
+      tileDataWrapperElement.appendChild(tileConditionTextElement)
       //
 
       // FAVOURITE BUTTON
-      const favouriteButtonWrapperElement = document.createElement('footer');
+      const favouriteButtonWrapperElement = document.createElement('footer')
 
-      const favouriteButtonElement = document.createElement('button');
-      const isThisCityFavourite = favouriteCities.includes(tile.cityName);
+      const favouriteButtonElement = document.createElement('button')
+      const isThisCityFavourite = favouriteCities.includes(tile.cityName)
       favouriteButtonElement.classList.add(
         'favourite-button',
         isThisCityFavourite ? 'remove' : 'add'
-      );
-      favouriteButtonElement.innerText = isThisCityFavourite ? '-' : '+';
+      )
+      favouriteButtonElement.innerText = isThisCityFavourite ? '-' : '+'
 
       favouriteButtonElement.onclick = () => {
         if (isThisCityFavourite) {
           favouriteCities = favouriteCities.filter(
             (city) => city !== tile.cityName
-          );
+          )
         } else {
-          favouriteCities.push(tile.cityName);
+          favouriteCities.push(tile.cityName)
         }
 
-        const currentShouldShowAll = getShouldShowAll();
-        const shouldDisplayImperial = getShouldDisplayImperial();
-        const searchQuery = getSearchQuery();
-        renderTiles(currentShouldShowAll, shouldDisplayImperial, searchQuery);
-      };
+        const currentShouldShowAll = getShouldShowAll()
+        const shouldDisplayImperial = getShouldDisplayImperial()
+        const searchQuery = getSearchQuery()
+        renderTiles(currentShouldShowAll, shouldDisplayImperial, searchQuery)
+      }
 
-      favouriteButtonWrapperElement.appendChild(favouriteButtonElement);
+      favouriteButtonWrapperElement.appendChild(favouriteButtonElement)
       //
 
       // TILE
-      const tileElement = document.createElement('div');
-      tileElement.className = 'tile';
-      tileElement.style.display = shouldThisTileBeVisible ? 'flex' : 'none';
+      const tileElement = document.createElement('div')
+      tileElement.className = 'tile'
+      tileElement.style.display = shouldThisTileBeVisible ? 'flex' : 'none'
 
-      tileElement.appendChild(tileHeaderElement);
-      tileElement.appendChild(tileDataWrapperElement);
-      tileElement.appendChild(favouriteButtonWrapperElement);
+      tileElement.appendChild(tileHeaderElement)
+      tileElement.appendChild(tileDataWrapperElement)
+      tileElement.appendChild(favouriteButtonWrapperElement)
       //
 
-      mainElement.appendChild(tileElement);
-    });
-  };
+      mainElement.appendChild(tileElement)
+    })
+  }
+  //
 
-  favouriteButtonElement.innerText = ALFA;
+  let favouriteCities = []
+
+  // CLEAR_BUTTON_ELEMENT
+  clearButtonElement.disabled = true
+  //
+
+  // FAVOURITE_BUTTON_ELEMENT
+  favouriteButtonElement.innerText = BUTTON_LABELS.showFavs
   favouriteButtonElement.onclick = () => {
-    const currentShouldShowAll = getShouldShowAll();
-    const shouldDisplayImperial = getShouldDisplayImperial();
-    const searchQuery = getSearchQuery();
-    const shouldShowFavsOnly = getShouldShowFavsOnly();
+    const currentShouldShowAll = getShouldShowAll()
+    const shouldDisplayImperial = getShouldDisplayImperial()
+    const searchQuery = getSearchQuery()
+    const shouldShowFavsOnly = getShouldShowFavsOnly()
 
     renderTiles(
       currentShouldShowAll,
       shouldDisplayImperial,
       searchQuery,
       shouldShowFavsOnly
-    );
-  };
+    )
+  }
+  //
 
-  moreButtonElement.innerText = CONTROL_BUTTON_LABEL.more;
+  // MORE_BUTTON_ELEMENT
+  moreButtonElement.innerText = BUTTON_LABELS.controls.more
   moreButtonElement.onclick = () => {
-    const currentShouldShowAll = getShouldShowAll();
+    console.log('onclick')
+
+    const shouldShowAll = !getShouldShowAll()
     moreButtonElement.innerText =
-      CONTROL_BUTTON_LABEL[currentShouldShowAll ? 'more' : 'less'];
-    const newShouldShowAll = !currentShouldShowAll;
-    const shouldDisplayImperial = getShouldDisplayImperial();
-    const searchQuery = getSearchQuery();
-    const shouldShowFavsOnly = getShouldShowFavsOnly();
+      BUTTON_LABELS.controls[shouldShowAll ? 'less' : 'more']
+    const shouldDisplayImperial = getShouldDisplayImperial()
+    const searchQuery = getSearchQuery()
+    const shouldShowFavsOnly = getShouldShowFavsOnly()
+
+    console.log({
+      shouldShowAll,
+      shouldDisplayImperial,
+      searchQuery,
+      shouldShowFavsOnly,
+    })
 
     renderTiles(
-      newShouldShowAll,
+      shouldShowAll,
       shouldDisplayImperial,
       searchQuery,
       shouldShowFavsOnly
-    );
-  };
+    )
+  }
+  //
 
-  unitSwitchButtonElement.innerText = UNITS_BUTTON_LABEL.imperial;
+  // UNIT_SWITCH_BUTTON_ELEMENT
+  unitSwitchButtonElement.innerText = BUTTON_LABELS.unitButtons.imperial
   unitSwitchButtonElement.onclick = () => {
-    const currentShouldDisplayImperial = getShouldDisplayImperial();
+    const currentShouldDisplayImperial = getShouldDisplayImperial()
     unitSwitchButtonElement.innerText =
-      UNITS_BUTTON_LABEL[currentShouldDisplayImperial ? 'imperial' : 'metric'];
-    const newShouldDisplayImperial = !currentShouldDisplayImperial;
-    const shouldShowAll = getShouldShowAll();
-    const searchQuery = getSearchQuery();
-    const shouldShowFavsOnly = getShouldShowFavsOnly();
+      BUTTON_LABELS.unitButtons[
+        currentShouldDisplayImperial ? 'imperial' : 'metric'
+      ]
+    const newShouldDisplayImperial = !currentShouldDisplayImperial
+    const shouldShowAll = getShouldShowAll()
+    const searchQuery = getSearchQuery()
+    const shouldShowFavsOnly = getShouldShowFavsOnly()
 
     renderTiles(
       shouldShowAll,
       newShouldDisplayImperial,
       searchQuery,
       shouldShowFavsOnly
-    );
-  };
+    )
+  }
+  //
 
-  searchButtonElement.disabled = true;
+  // SEARCH_BUTTON_ELEMENT
+  searchButtonElement.disabled = true
   searchButtonElement.onclick = () => {
-    const searchQuery = getSearchQuery();
-    const shouldShowAll = getShouldShowAll();
-    const shouldDisplayImperial = getShouldDisplayImperial();
-    const shouldShowFavsOnly = getShouldShowFavsOnly();
+    const searchQuery = getSearchQuery()
+    const shouldShowAll = getShouldShowAll()
+    const shouldDisplayImperial = getShouldDisplayImperial()
+    const shouldShowFavsOnly = getShouldShowFavsOnly()
 
     renderTiles(
       shouldShowAll,
       shouldDisplayImperial,
       searchQuery,
       shouldShowFavsOnly
-    );
-  };
+    )
+  }
+  //
 
-  const handleSearchFieldElementOnChange = () => {
-    const currentNumberOfCharsInSearchField = searchFieldElement.value.length;
+  // INPUT_EVENT_LISTENER
+  searchFieldElement.addEventListener('input', () => {
+    const currentNumberOfCharsInSearchField = searchFieldElement.value.length
 
-    searchButtonElement.disabled = !(currentNumberOfCharsInSearchField > 2);
-    clearButtonElement.disabled = !currentNumberOfCharsInSearchField;
-  };
-  searchFieldElement.addEventListener(
-    'input',
-    handleSearchFieldElementOnChange
-  );
+    searchButtonElement.disabled = !(currentNumberOfCharsInSearchField > 2)
+    clearButtonElement.disabled = !currentNumberOfCharsInSearchField
+  })
 
-  renderTiles();
-})();
+  // INITAL_TILES_RENDER
+  renderTiles()
+})()
