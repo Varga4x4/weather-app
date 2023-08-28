286410f06267fcae2efb79322538d746b2e2618d
unknown <grandalf6@gmail.com>
2023-08-28 13:43:12 +0100

    Added missing features section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // -* Data should comming from https://www.weatherapi.com/docs/
-//
+
+// MISSING FEATURES
+// - 'X' button not working (should show tiles without search query filter applied)
+// - 'Show favourites' button not working (should show tiles marked as favs only)
+
 // HOME WORK
 // - Next to units, there should be button 'Show' favs', after pressing it should become 'Show all' and only favs cities should be shown in grid (units, more/less and searching should still work on favs)
 
